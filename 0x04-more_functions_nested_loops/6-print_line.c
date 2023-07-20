50fc7cb80ae3cd18761de9faff8dc78a4a21cf90
Johnkgabo <Johnkgabo@gmail.com>
2023-07-20 11:10:55 -0700

    Numbers constitute the only universal language

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_line - prints shortest distance between two points is a straight line
+ * Return: void
+ */
+void print_line(int n)
+{
+	int n;
+
+	for (n < 0)
+}
