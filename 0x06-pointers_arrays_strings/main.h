fc1a697974008470c971dfb979f3a69f5a7bbbec
Johnkgabo <Johnkgabo@gmail.com>
2023-07-26 23:57:12 -0700

    strncat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
@@ -15,5 +14,5 @@ void print_buffer(char *b, int size);
 char *leet(char *n);
 char *rot13(char *s);
 int _putchar(char c);
-
+char *_strncat(char *dest, char *src, int n);
 #endif
